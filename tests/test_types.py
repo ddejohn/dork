3d5bb62dc45d7f6ebc9091eeac12800264a8d807
Devon DeJohn <ddejohn@msudenver.edu>
2019-07-26 18:05:11 -0600

    code cleanup

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,56 +1,33 @@
 # -*- coding: utf-8 -*-
-"""Basic tests for state and entity relationships in dork
-"""
-from tests.utils import is_a
+"""Basic tests for state and entity relationships in dork"""
+
+
 import dork.types as types
 import dork.game_utils.factory_data as factory_data
-
 # pylint: disable=protected-access
-
-
-def test_confirm_method_yes(capsys, mocker):
-    """confirm should do things"""
-
-    mocked_input = mocker.patch('builtins.input')
-    mocked_input.side_effect = ["y", "tester"]
-    assert types.Game._confirm()
-    captured = capsys.readouterr()
-    assert "\n!!!WARNING!!! You will lose unsaved data!\n" in captured.out
-    assert mocked_input.call_count == 1
-
-
-def test_confirm_method_no(capsys, mocker):
-    """confirm should do things"""
-
-    mocked_input = mocker.patch('builtins.input')
-    mocked_input.side_effect = ["n"]
-    assert types.Game._confirm() == "guess you changed your mind!"
-    captured = capsys.readouterr()
-    assert "\n!!!WARNING!!! You will lose unsaved data!\n" in captured.out
-    assert mocked_input.call_count == 1
 
 
 def test_confirm_method_blank(capsys, mocker):
     """confirm should do things"""
 
     mocked_input = mocker.patch('builtins.input')
-    mocked_input.side_effect = ["afk", "    ", "y", "tester"]
+    mocked_input.side_effect = ["bumblebee", "y", "tester"]
     types.Game._confirm()
     captured = capsys.readouterr()
     assert "\n!!!WARNING!!! You will lose unsaved data!\n" in captured.out
     assert "That is not a valid response!" in captured.out
-    assert mocked_input.call_count == 3
+    assert mocked_input.call_count == 2
 
 
 def test_start_over_no(capsys, mocker, game):
     """confirm should do things"""
 
     mocked_input = mocker.patch('builtins.input')
-    mocked_input.side_effect = ["n", ".rq"]
+    mocked_input.side_effect = ["bumblebee", "n"]
     assert game._start_over() == ("guess you changed your mind!", False)
     captured = capsys.readouterr()
     assert "\n!!!WARNING!!! You will lose unsaved data!\n" in captured.out
-    assert mocked_input.call_count == 1
+    assert mocked_input.call_count == 2
 
 
 def test_start_over_yes(capsys, mocker, game):
@@ -65,34 +42,19 @@ def test_start_over_yes(capsys, mocker, game):
     assert mocked_input.call_count == 1
 
 
-def test_player_location(game):
-    """testing the get and set of player location"""
-
-    is_a(game.hero.location, types.Room)
-
-
 def test_move_method(game, cardinals):
     """testing the move function for any map"""
 
     for direction in cardinals:
-        if getattr(game.hero.location, direction) is not None:
-            move_return = game._move(direction)
-            assert (game.hero.location.description, False) == move_return
-        if not getattr(game.hero.location, direction):
-            move_return = game._move(direction)
-            assert (
-                f"You cannot go {direction} from here.", False) == move_return
-
-
-def test_factory_data():
-    """test factory data methods"""
-
-    assert isinstance(factory_data.rules(0, 0), list)
-    assert isinstance(factory_data.stats("magic"), dict)
+        assert game._move(direction) in [
+            (game.hero.location.description, False),
+            (f"You cannot go {direction} from here.", False)
+        ]
 
 
 def test_mazefactory():
     """builds all game types"""
 
-    new_game = types.MazeFactory.build()
-    assert isinstance(new_game, dict)
+    assert isinstance(factory_data.rules(0, 0), list)
+    assert isinstance(factory_data.stats("magic"), dict)
+    assert isinstance(types.MazeFactory.build(), dict)
