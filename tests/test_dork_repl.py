f55834e03108de4dbbe2581b87ff9fe6c8dfa7d9
Devon DeJohn <ddejohn@msudenver.edu>
2019-07-27 20:55:32 -0600

    GTFO

--- tests/test_dork_repl.py
+++ tests/test_dork_repl.py
@@ -121,3 +121,24 @@ def test_repl_bad_keys(game):
     assert repl._evaluate("take LARSEN", game) == (
         "There is no LARSEN here.", False
     )
+
+
+def test_basic_command_functioning(game):
+    """Basic commands must not crash game"""
+
+    assert repl._evaluate("go", game)
+    assert repl._evaluate("move", game)
+    assert repl._evaluate("walk", game)
+    assert repl._evaluate("travel", game)
+    assert repl._evaluate("run", game)
+    assert repl._evaluate("head", game)
+    assert repl._evaluate("look", game)
+    assert repl._evaluate("i", game)
+    assert repl._evaluate("inv", game)
+    assert repl._evaluate("inventory", game)
+    assert repl._evaluate("examine", game)
+    assert repl._evaluate("add", game)
+    assert repl._evaluate("grab", game)
+    assert repl._evaluate("take", game)
+    assert repl._evaluate("loot", game)
+    assert repl._evaluate("drop", game)
